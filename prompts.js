33f3f8e4b2086a4fc3ce2c26214e96f3be9abfb8
rigor789 <rigor789@gmail.com>
2018-05-11 14:11:19 +0200

    add app id prompt and validator

---
+++ prompts.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: 'applicationId',
+    type: 'input',
+    message: 'Enter a unique application identifier:',
+    default: 'org.nativescript.application',
+    validate(applicationId) {
+      const idRE = /^([A-Za-z][A-Za-z\d_]*\.)+[A-Za-z][A-Za-z\d_]*$/
+
+      if (!idRE.test(applicationId)) {
+        return `Invalid application identifier.
+A valid identifier:
+ - must contain two or more strings separated by a dot
+ - each string must start with a letter
+ - each string can only contain numbers, letters and the _ character
+Example: com.company.app`
+      }
+
+      return true;
+    }
+  }
+]
