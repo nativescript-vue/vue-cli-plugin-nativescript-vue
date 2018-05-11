9fdc21d43e425095d0d47f11b0e50d2cb9fcc538
rigor789 <rigor789@gmail.com>
2018-05-11 16:38:17 +0200

    add app template, tweak webpack config

--- generator/index.js
+++ generator/index.js
@@ -13,6 +13,7 @@ module.exports = (api, options, rootOptions) => {
     devDependencies: {
       'nativescript-vue-loader': '1.0.0',
       'nativescript-vue-template-compiler': '^1.3.1',
+      'tns-core-modules': '^4.0.0'
     }
   })
 
@@ -20,9 +21,17 @@ module.exports = (api, options, rootOptions) => {
     delete pkg.dependencies['vue']
     delete pkg.devDependencies['vue-template-compiler']
     delete pkg.browserslist
+    delete pkg.scripts['serve']
   })
 
-  api.render('./templates/simple')
+  api.render('./templates/simple', {
+    applicationName: api.generator.pkg.name,
+    applicationVersion: api.generator.pkg.version,
+    applicationAndroidVersionCode: api.generator.pkg.version.split('.').join('0'),
+    applicationDescription: api.generator.pkg.description || api.generator.pkg.name,
+    applicationLicense: api.generator.pkg.license || 'MIT',
+    applicationId: options.applicationId,
+  })
 
   // delete the "public" directory
   api.onCreateComplete(() => {
--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const path = require('path')
+
 module.exports = (api, projectOptions) => {
+  process.env.VUE_CLI_TARGET = 'nativescript'
+
   api.chainWebpack(config => {
+    config.output.filename(`app.js`)
+      .chunkFilename('vendor.js')
+
     config.resolve.alias.delete('vue$');
+
+    config.resolve.extensions.add('.android.js')
+    config.resolve.extensions.add('.common.js')
+
+    const tnsCorePath = api.resolve('node_modules/tns-core-modules')
+    config.resolve.modules.add(tnsCorePath)
+    config.resolveLoader.modules.add(tnsCorePath)
+
     const vueRule = config.module.rule('vue').test(/\.vue$/)
 
     vueRule.use('vue-loader').tap(options => {
@@ -17,6 +32,31 @@ module.exports = (api, projectOptions) => {
     addLoader({
       loader: 'nativescript-vue-loader',
       options: {}
+    })
+
+    config.plugins.delete('hmr')
+    config.plugin('extract-css').tap(args => {
+      if (!args.length) return args;
+
+      args[0].filename = 'app.css'
+      args[0].chunkFilename = 'vendor.css'
+
+      return args;
+    })
+
+    const nodeModulesPath = api.resolve('node_modules')
+    config.externals((context, request, callback) => {
+      if (context.startsWith(nodeModulesPath)) {
+        const module = context.replace(nodeModulesPath, '').split(path.sep).find(p => !!p)
+        try {
+          const pkg = require(path.resolve(nodeModulesPath, module, 'package.json'))
+          if(pkg.nativescript) {
+            return callback(null, 'commonjs ' + request)
+          }
+        } catch (e) {
+        }
+      }
+      callback()
     })
   })
 
