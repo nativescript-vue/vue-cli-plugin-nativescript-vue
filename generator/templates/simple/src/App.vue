f023847365598a632f2372e10ab659af1ba28754
Jawa-the-Hutt <jawa-the-hutt@users.noreply.github.com>
2018-11-03 17:06:10 -0500

    Initial Commit

--- generator/templates/simple/src/App.vue
+++ generator/templates/simple/src/App.vue
@@ -1,36 +1,89 @@
 <%_ if (!rootOptions.router) { _%>
-<template>
+<template native>
   <Page>
-    <ActionBar title="NativeScript-Vue"/>
+    <ActionBar :title="navbarTitle"/>
     <GridLayout rows="auto, auto">
-        <Image src="./assets/logo.png" row="0" />
+        <Image src="~/assets/logo.png" row="0" />
         <HelloWorld msg="Welcome to Your Vue.js App" row="1" />
     </GridLayout>
   </Page>
 </template>
-
+<template web>
+  <div class="w-page">
+    <nav>
+      <ul class="w-navbar">
+          <li class="w-title" :text="navbarTitle">{{navbarTitle}}</li>
+      </ul>
+    </nav>
+    <div class="w-container">
+      <img src="@/assets/logo.png" alt="logo" height="20%" width="20%">
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+  </div>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // ~ is an alias to /src
+  import HelloWorld from '~/components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        navbarTitle: process.env.TNS_APP_MODE === 'native' ? 'NativeScript-Vue' : 'Web Vue',
+      }
+    },   
   }
-}
-</script>
+  </script>
 <%_ } else { _%>
-<template>
+<template native>
   <Page>
-    <ActionBar title="NativeScript-Vue"/>
-    <GridLayout rows="auto, auto, *">
-        <Button text="Home" @tap="$router.replace('/')" />
-        <Button text="About" @tap="$router.replace('/about')" />
-
-        <router-view row="2" />
+    <ActionBar title="NativeScript-Vue - App.vue"/>
+    <GridLayout rows="auto, auto">
+        <Button text="Home" @tap="goToHomePage" row="0"/>
+        <Button text="About" @tap="goToAboutPage" row="1"/>
     </GridLayout>
   </Page>
 </template>
+<template web>
+  <div class="w-page">
+    <nav>
+      <ul class="w-navbar">
+          <li class="w-title" :text="navbarTitle">{{navbarTitle}}</li>
+      </ul>
+    </nav>
+    <div class="w-container">
+      <router-link tag="button" class="w-button" id="homeButton" to="/">Home</router-link>
+      <router-link tag="button" class="w-button" id="aboutButton" to="/about">About</router-link>
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+  // ~ is an alias to /src
+  import Home from '~/views/Home.vue';
+  import About from '~/views/About.vue';
+
+  export default {
+
+    data() {
+      return {
+        navbarTitle: process.env.TNS_APP_MODE === 'native' ? 'NativeScript-Vue' : 'Web Vue',
+      }
+    },    
+    methods: {
+      goToHomePage() {
+        process.env.TNS_APP_MODE === 'native' ? this.$navigateTo(Home) : null;
+      },
+      goToAboutPage() {
+        process.env.TNS_APP_MODE === 'native' ? this.$navigateTo(About): null;
+      }
+    }
+  }
+
+</script>
 <%_ } _%>
 
 <%_ if (rootOptions.cssPreprocessor !== 'stylus') { _%>
@@ -43,13 +96,112 @@ export default {
       }"`
     : ``
 %>>
-ActionBar {
-  color: #42b983;
-}
+  ActionBar, .w-navbar {
+    color: #42b983;
+  }
+
+  .w-page {
+    height: 100%;
+    width: 100%;
+  }
+
+  .w-navbar {
+    position: fixed;
+    z-index: 10000;
+    height: 3em;
+    width: 100%;
+    top: 0px;
+    left: 0px;
+    margin: auto;
+    list-style: none;
+
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+
+    -webkit-box-shadow: -8px 8px 6px -7px #999;
+    -moz-box-shadow: -8px 8px 6px -7px #999;
+    box-shadow: -8px 8px 6px -7px #999;
+
+    .w-title {
+        margin-left: auto;
+        margin-right: auto;
+    }
+  }
+
+  .w-container {
+    height: 100%;
+    width: 100%;
+    padding-top: 3em;
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;      
+    justify-content: top;  
+    align-items: center;   
+  
+
+    .w-button {
+      width: 50%;
+      height: 2em;
+      margin: .25em;
+      display: flex;  
+      justify-content: center;  
+      align-items: center;
+      background-color: #d7d7d7;
+      border-width: 0px;
+      font-weight: 600;
+      border-radius: 3px;
+    }
+    
+  }
+
 </style>
 <%_ } else { _%>
 <style lang="stylus">
-ActionBar
-  color #42b983
+  ActionBar, .w-navbar
+    color: #42b983
+  .w-page
+    height: 100%
+    width: 100%
+  .w-navbar
+    position: fixed
+    z-index: 10000
+    height: 3em
+    width: 100%
+    top: 0px
+    left: 0px
+    margin: auto
+    list-style: none
+    display: flex
+    align-items: center
+    padding: 0 10px
+    -webkit-box-shadow: -8px 8px 6px -7px #999
+    -moz-box-shadow: -8px 8px 6px -7px #999
+    box-shadow: -8px 8px 6px -7px #999
+    .w-title
+      margin-left: auto
+      margin-right: auto
+  .w-container
+    height: 100%
+    width: 100%
+    padding-top: 3em
+    position: relative
+    overflow: hidden
+    display: flex
+    flex-direction: column
+    justify-content: top
+    align-items: center
+    .w-button
+      width: 50%
+      height: 2em
+      margin: .25em
+      display: flex
+      justify-content: center
+      align-items: center
+      background-color: #d7d7d7
+      border-width: 0px
+      font-weight: 600
+      border-radius: 3px
 </style>
 <%_ } _%>
--- index.js
+++ index.js
@@ -1,65 +1,472 @@
 const path = require('path')
 
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+
+const nativescriptTarget = require("nativescript-dev-webpack/nativescript-target");
+const NsVueTemplateCompiler = require("nativescript-vue-template-compiler");
+const nsWebpack = require("nativescript-dev-webpack");
+const PlatformFSPlugin = nsWebpack.PlatformFSPlugin;
+const WatchStateLoggerPlugin = nsWebpack.WatchStateLoggerPlugin;
+const { NativeScriptWorkerPlugin } = require("nativescript-worker-loader/NativeScriptWorkerPlugin");
+
 module.exports = (api, projectOptions) => {
+
+  const env = process.env.NODE_ENV;
+  const platform = process.env.VUE_PLATFORM //&& (process.env.VUE_PLATFORM && "android" || process.env.VUE_PLATFORM && "ios" || process.env.VUE_PLATFORM && "web");
+  const appMode = platform === 'android' ? 'native' : platform === 'ios' ? 'native' : 'web';
+  process.env.VUE_APP_MODE = appMode;
+  
+  projectOptions.outputDir = path.join(api.service.context, appMode === 'web' ? 'platforms/web' : nsWebpack.getAppPath(platform, api.service.context));
+
+  return appMode === 'web' ? webConfig(api, projectOptions, env, appMode) : nativeConfig(api, projectOptions, env, platform);
+
+}
+
+const nativeConfig = (api, projectOptions, env, platform) => {
   process.env.VUE_CLI_TARGET = 'nativescript'
 
+  const appComponents = [
+    "tns-core-modules/ui/frame",
+    "tns-core-modules/ui/frame/activity",
+  ];
+
+  const tnsCorePath = api.resolve('node_modules/tns-core-modules')
+
+  const {
+    // The 'appPath' and 'appResourcesPath' values are fetched from
+    // the nsconfig.json configuration file
+    // when bundling with `tns run android|ios --bundle`.
+    appPath = api.resolve('src'),
+    appResourcesPath = path.join(appPath, './App_Resources'),
+
+    // You can provide the following flags when running 'tns run android|ios'
+    snapshot,
+    production, 
+    report, 
+    hmr, 
+  } = env;  
+
+  const platforms = ["ios", "android"];
+  const appResourcesPlatformDir = platform === "android" ? "Android" : "iOS";
+
+  const appFullPath = appPath;
+  const appResourcesFullPath = appResourcesPath;
+
+  const entryModule = nsWebpack.getEntryModule(appFullPath);
+  const entryPath = `.${path.sep}${entryModule}.js`;
+
   api.chainWebpack(config => {
-    config.output.filename(`app.js`)
-      .chunkFilename('vendor.js')
 
-    config.resolve.alias.delete('vue$');
+    config
+      .mode(env)
+      .devtool('none')
+      .context(appFullPath)
+      .end();
 
-    config.resolve.extensions.add('.android.js')
-    config.resolve.extensions.add('.common.js')
+    config
+      .watchOptions({
+          ignored: [
+              appResourcesFullPath,
+              // Don't watch hidden files
+              "**/.*",
+          ],
+      })
+      .target(nativescriptTarget)
+      .end();
 
-    const tnsCorePath = api.resolve('node_modules/tns-core-modules')
-    config.resolve.modules.add(tnsCorePath)
-    config.resolveLoader.modules.add(tnsCorePath)
+    config
+      // clear out old config.entryPoints and install new
+      .entryPoints
+          .clear()
+          .end()
+      .entry('bundle')
+          .add(entryPath)
+      .end();
 
-    const vueRule = config.module.rule('vue').test(/\.vue$/)
+    // clear out old config.output and install new
+    config
+      .output.clear()
+      .end()
+      .output
+          .path(projectOptions.outputDir)
+          .pathinfo(false)
+          .libraryTarget('commonjs2')
+          .globalObject('global')
+          .filename(`[name].js`)
+          .end();
 
-    vueRule.use('vue-loader').tap(options => {
-      options.compilerOptions.compiler = require('nativescript-vue-template-compiler')
-      options.compilerOptions = options.compilerOpitons
-      delete options.compilerOpitons;
+   config
+        .resolve
+            .extensions
+                .add('.scss')
+                .add('.css')
+                .add('.android.js')
+                .add('.common.js')
+                .end()
+            // Resolve {N} system modules from tns-core-modules
+            .modules
+                .add(path.resolve(api.service.context, tnsCorePath))
+                .add(tnsCorePath)
+                .add('node_modules/tns-core-modules')
+                .add('node_modules')
+                .end()
+            .alias
+                .delete('vue$')
+                .set('~', appFullPath)
+                .set('vue$', 'nativescript-vue')
+                .end()
+            .symlinks(false)  // don't resolve symlinks to symlinked modules
+            .end();
+
+    config
+        .resolveLoader
+            .symlinks(false)  //  don't resolve symlinks to symlinked modules
+            .modules
+              .add(tnsCorePath)
+              .end()
+            .end();
+
+    config
+        .node
+          .set('setImmediate', false)
+          .set('http', false)
+          .set('timers', false)
+          .set('fs', 'empty')
+          .set('__dirname', false)
+          .end();
+
+
+    config.optimization
+      .minimize(Boolean(production))
+      .end()
+
+    config.optimization
+      .splitChunks( {
+          cacheGroups: {
+              vendor: {
+                  name: "vendor",
+                  chunks: "all",
+                  test: (module) => {
+                      const moduleName = module.nameForCondition ? module.nameForCondition() : '';
+                      return /[\\/]node_modules[\\/]/.test(moduleName) ||
+                          appComponents.some(comp => comp === moduleName);
+
+                  },
+                  enforce: true,
+              },
+          },
+      })
+      .end()
+
+    config.optimization
+      .minimizer([
+          new TerserPlugin({
+            parallel: true,
+            cache: true,
+            terserOptions: {
+                output: {
+                    comments: false,
+                },
+                compress: {
+                    // The Android SBG has problems parsing the output
+                    // when these options are enabled
+                    'collapse_vars': platform !== "android",
+                    sequences: platform !== "android",
+                },
+                safari10: platform === "ios",
+                keep_fnames: true,
+            },
+        }),        
+      ])
+      .end()
+
+    config.module
+      .rule('native-loaders')
+        .test(new RegExp(entryPath))
+        .use('nativescript-dev-webpack/bundle-config-loader')
+          .loader('nativescript-dev-webpack/bundle-config-loader')
+          .options({
+            registerPages: true, // applicable only for non-angular apps
+            loadCss: !snapshot, // load the application css if in debug mode
+          })       
+        .end()  
+
+    config.when(platform === 'android', config => {
+      config.module
+        .rule('native-loaders')    
+          .use('nativescript-dev-webpack/android-app-components-loader')
+            .loader('nativescript-dev-webpack/android-app-components-loader')
+            .options({
+              modules: appComponents
+            })
+            .before('nativescript-dev-webpack/bundle-config-loader')
+            .end()
     })
+    
+    // delete the vue loader rule and rebuid it
+    config.module.rules.delete('vue')
+    config.module
+      .rule('vue')
+        .test(/\.vue$/)
+        .use('vue-loader')
+          .loader('vue-loader')
+          .options(Object.assign({
+              compiler: NsVueTemplateCompiler,
+          }, {}))
+          .before('string-replace-loader')
+          .end()
+        .use('string-replace-loader')
+          .loader('string-replace-loader')
+          .options(Object.assign({
+            search: '<template web>[\\s\\S]*?<\/template>',
+            replace: '',
+            flags: 'gim',
+            strict: true
+          }, {}))
+          .end()
 
-    const addLoader = ({loader, options}) => {
-      vueRule.use(loader).loader(loader).options(options)
-    }
 
-    addLoader({
-      loader: 'nativescript-vue-loader',
-      options: {}
-    })
+    // delete the js loader rule and rebuid it
+    config.module.rules.delete('js')
+    config.module
+      .rule('js')
+        .test(/\.jsx?$/)
+        .use('babel-loader')
+          .loader('babel-loader')
+          .end()
 
+    // delete the css loader rule and rebuid it
+    config.module.rules.delete('css')
+    config.module
+      .rule('css')
+        .test(/\.css$/)
+        .use('nativescript-dev-webpack/style-hot-loader')
+          .loader('nativescript-dev-webpack/style-hot-loader')
+          .before('nativescript-dev-webpack/apply-css-loader')
+          .end()
+        .use('nativescript-dev-webpack/apply-css-loader')
+          .loader('nativescript-dev-webpack/apply-css-loader')
+          .before('css-loader')
+          .end()
+        .use('css-loader')
+          .loader('css-loader')
+          .options(Object.assign({
+            minimize: false, 
+            url: false,
+        }, {}))
+        .end()
+
+
+    // delete the scss rule and rebuid it
+    config.module.rules.delete('scss')
+      config.module
+        .rule('scss')
+          .test(/\.scss$/)
+          .use('nativescript-dev-webpack/style-hot-loader')
+            .loader('nativescript-dev-webpack/style-hot-loader')
+            .before('nativescript-dev-webpack/apply-css-loader')
+            .end()
+          .use('nativescript-dev-webpack/apply-css-loader')
+            .loader('nativescript-dev-webpack/apply-css-loader')
+            .before('css-loader')
+            .end()
+          .use('css-loader')
+            .loader('css-loader')
+            .options(Object.assign({
+              minimize: false, 
+              url: false,
+            }, {}))
+            .before()
+            .end()
+          .use('sass-loader')
+            .loader('sass-loader')
+
+
+    // delete these rules that come standard with CLI 3
+    // need to look at adding these back in after evaluating impact
+    config.module.rules.delete('images')
+    config.module.rules.delete('svg')
+    config.module.rules.delete('media')
+    config.module.rules.delete('fonts')
+    config.module.rules.delete('pug')
+    config.module.rules.delete('postcss')
+    config.module.rules.delete('sass')
+    config.module.rules.delete('less')
+    config.module.rules.delete('stylus')
+    config.module.rules.delete('eslint')
+    .end();
+
+
+    // delete these plugins that come standard with CLI 3
     config.plugins.delete('hmr')
-    config.plugin('extract-css').tap(args => {
-      if (!args.length) return args;
+    config.plugins.delete('html')
+    config.plugins.delete('preload')
+    config.plugins.delete('prefetch')
+    config.plugins.delete('pwa')
+    config.plugins.delete('progress')
+    config.plugins.delete('copy')
+    .end();
 
-      args[0].filename = 'app.css'
-      args[0].chunkFilename = 'vendor.css'
+    // create new plugins
 
-      return args;
-    })
-
-    const nodeModulesPath = api.resolve('node_modules')
-    config.externals((context, request, callback) => {
-      if (context.startsWith(nodeModulesPath)) {
-        const module = context.replace(nodeModulesPath, '').split(path.sep).find(p => !!p)
-        try {
-          const pkg = require(path.resolve(nodeModulesPath, module, 'package.json'))
-          if(pkg.nativescript) {
-            return callback(null, 'commonjs ' + request)
+    // Define useful constants like TNS_WEBPACK
+    config.plugin('define')
+      .use(DefinePlugin, [{
+          "global.TNS_WEBPACK": "true",
+          'process.env': {
+            "TNS_ENV": JSON.stringify(env),
+            'TNS_PLATFORM': JSON.stringify(platform),
+            'TNS_APP_MODE': JSON.stringify(process.env.VUE_APP_MODE)
           }
-        } catch (e) {
-        }
-      }
-      callback()
-    })
+      }])
+      .end()
+
+    // Remove all files from the out dir.
+    config.plugin('clean')
+      .use(CleanWebpackPlugin, [path.join(projectOptions.outputDir, '/**/*'), {root: projectOptions.outputDir}])
+      .end();
+
+    // Copy native app resources to out dir.
+    config.plugin('copy-native-resources')
+      .use(CopyWebpackPlugin, [
+        [{
+          from: path.join(appResourcesFullPath, appResourcesPlatformDir),
+          to: path.join(projectOptions.outputDir, 'App_Resources', appResourcesPlatformDir ),
+          context: api.service.context,
+        }]
+      ])
+      .end();
+
+    // Copy assets to out dir. Add your own globs as needed.
+    config.plugin('copy-assets')
+      .use(CopyWebpackPlugin, [[
+          { from: "fonts/**"},
+          { from: "**/*.+(jpg|png)"},
+          { from: "assets/**/*"},
+        ],{ ignore: [path.join(appResourcesFullPath, '**')]}
+      ])
+      .end();
+
+    // Generate a bundle starter script and activate it in package.json
+    config.plugin('generate-bundle-starter')
+      .use(nsWebpack.GenerateBundleStarterPlugin, [[
+        './vendor',
+        './bundle'
+      ]])
+      .end();
+
+    // For instructions on how to set up workers with webpack
+    // check out https://github.com/nativescript/worker-loader
+    config.plugin('nativescript-worker')
+      .use(NativeScriptWorkerPlugin, [])
+      .end();
+
+    config.plugin('platform-FS')
+      .use(PlatformFSPlugin, [{
+        platform,
+        platforms,
+      }])
+      .end();
+
+    // Does IPC communication with the {N} CLI to notify events when running in watch mode.
+    config.plugin('watch-state-logger')
+      .use(WatchStateLoggerPlugin, [])
+      .end();
+
+    // // // // causes error on compile at the moment
+    // // // config.plugin('extract-css').tap(args => {
+    // // //   if (!args.length) return args;
+
+    // // //   args[0].filename = 'app.css'
+    // // //   args[0].chunkFilename = 'vendor.css'
+
+    // // //   return args;
+    // // // })
+
+    // // // // causes error on compile at the moment
+    // // // const nodeModulesPath = api.resolve('node_modules')
+    // // // config.externals((context, request, callback) => {
+    // // //   if (context.startsWith(nodeModulesPath)) {
+    // // //     const module = context.replace(nodeModulesPath, '').split(path.sep).find(p => !!p)
+    // // //     try {
+    // // //       const pkg = require(path.resolve(nodeModulesPath, module, 'package.json'))
+    // // //       if(pkg.nativescript) {
+    // // //         return callback(null, 'commonjs ' + request)
+    // // //       }
+    // // //     } catch (e) {
+    // // //     }
+    // // //   }
+    // // //   callback()
+    // // // })
   })
 
-  api.registerCommand('tns', {
+  setupCommands(api);
+
+}
+
+const webConfig = (api, projectOptions, env, appMode) => {
+
+  api.chainWebpack(config => {
+
+    config.entry('app').clear()
+    config.entry('app').add(path.resolve(api.resolve('src'), 'main.web.js'));
+
+    config
+      .output
+          .path(projectOptions.outputDir)
+          .end();
+
+    config.resolve.alias.set('~', api.resolve('src'))
+
+    config.module
+        .rule('vue')
+            .use('cache-loader')
+                .loader('cache-loader')
+                .tap(options => {
+                    options.cacheDirectory = config.module.rule('vue').uses.get('cache-loader').get('options').cacheDirectory + '\\' + appMode;
+                    return options;
+                })
+                .end()
+            .use('vue-loader')
+                .loader('vue-loader')
+                .options(Object.assign({
+                    //compiler: NsVueTemplateCompiler,
+                }, config.module.rule('vue').uses.get('vue-loader').get('options')))
+                .end()
+            .use('string-replace-loader')
+                .loader('string-replace-loader')
+                .options({
+                    search: '<template native>[\\s\\S]*?<\/template>',
+                    replace: '',
+                    flags: 'gim',
+                    strict: true,                 
+                })
+
+    // Define useful constants like TNS_WEBPACK
+    config.plugin('define')
+      .use(DefinePlugin, [{
+        'process.env': {
+          "TNS_ENV": JSON.stringify(env),
+          'TNS_PLATFORM': JSON.stringify('web'),
+          'TNS_APP_MODE': JSON.stringify('web')
+        }
+      }])
+      .end()
+
+    // Remove all files from the out dir.
+    config.plugin('clean')
+      .use(CleanWebpackPlugin, [path.join(projectOptions.outputDir, '/**/*'), {root: projectOptions.outputDir}] )
+      .end();
+
+  })
+}
+
+
+setupCommands = (api) => {
+  api.registerCommand('tns:dev', {
     description: 'run nativescript cli commands',
     usage: 'vue-cli-service tns [options]',
     options: {
@@ -67,5 +474,45 @@ module.exports = (api, projectOptions) => {
       '--ios': 'run in ios simulator',
       '--release': 'run in release mode',
     }
-  }, require('./lib/commands/tns'))
+  }, args => { 
+    return require('./lib/commands/tns')(args, api)
+  })
+
+  api.registerCommand('tns:prod', {
+    description: 'run nativescript cli commands',
+    usage: 'vue-cli-service tns [options]',
+    options: {
+      '--android': 'run in android emulator',
+      '--ios': 'run in ios simulator',
+      '--release': 'run in release mode',
+    }
+  }, args => {
+    return require('./lib/commands/tns')(args, api)
+  })
+
+  // // NOT IMPLEMENTED AT ALL
+  // api.registerCommand('tns:snapshot', {
+  //   description: 'run nativescript cli commands',
+  //   usage: 'vue-cli-service tns [options]',
+  //   options: {
+  //     '--android': 'run in android emulator',
+  //     '--ios': 'run in ios simulator',
+  //     '--release': 'run in release mode',
+  //   }
+  // }, args => { 
+  //   return require('./lib/commands/tns')(args, api)
+  // })
+
+  // // NOT IMPLEMENTED AT ALL
+  // api.registerCommand('tns:report', {
+  //   description: 'run nativescript cli commands',
+  //   usage: 'vue-cli-service tns:report [options]',
+  //   options: {
+  //     '--android': 'run in android emulator',
+  //     '--ios': 'run in ios simulator',
+  //     '--release': 'run in release mode',
+  //   }
+  // }, args => { 
+  //   return require('./lib/commands/tns')(args, api)
+  // })
 }
