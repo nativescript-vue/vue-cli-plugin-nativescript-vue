b48559500279fecdbeff69a11cf9804487274a55
jawa-the-hutt <jawa-the-hutt@users.noreply.github.com>
2019-01-31 07:36:48 -0600

    Updated generic SFC template with latest structure

--- generator/templates/vue-sfc-template.vue
+++ generator/templates/vue-sfc-template.vue
@@ -1,14 +1,28 @@
+<%_ if (rootOptions.router) { _%>
+<%# -------------------- IS Using vue-router  -------------------- -%>
 <template></template>
-<%_ if (!usingTS) { _%>
-<%# -------------------- Is Not Using TypeScript  -------------------- -%>
+<%_ } else { _%>
+<%# -------------------- IS NOT Using vue-router  -------------------- -%>
+<template></template>
+<%_ } _%>
+<%_ if (!usingTS && rootOptions.router) { _%>
+<%# -------------------- IS NOT Using TypeScript AND IS Using vue-router  -------------------- -%>
 <%# Remove this line and Uncomment the next to use script tag - Prettier formatting bug removes all script tags if these are left in -%>
 <%# <script></script> -%>
-<%_ } else { _%>
-<%# -------------------- Is Using TypeScript  -------------------- -%>
+<%_ } else if (!usingTS && !rootOptions.router) { _%>
+<%# -------------------- IS NOT Using TypeScript AND IS NOT Using vue-router  -------------------- -%>
 <%# Remove this line and Uncomment the next to use script tag - Prettier formatting bug removes all script tags if these are left in -%>
+<%# <script></script> -%>
+<%_ } else if (usingTS && rootOptions.router) { _%>
+<%# -------------------- IS Using TypeScript AND IS Using vue-router  -------------------- -%>
 <%# <script lang="ts"></script> -%>
+<%_ } else if (usingTS && !rootOptions.router) { _%>
+<%# -------------------- IS Using TypeScript AND IS NOT Using vue-router  -------------------- -%>
+<%# <script lang="ts"></script> -%>
+<%_ } else { _%>
+<%# -------------------- don't do anything -------------------- -%>
 <%_ } _%>
-<!-- Add "scoped" attribute to limit CSS to this component only -->	
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <%_ if (rootOptions.cssPreprocessor) { _%>
 <%_   if (rootOptions.cssPreprocessor !== 'stylus') { _%>
 <%#   -------------------- IS Using scss OR sass -------------------- -%>
